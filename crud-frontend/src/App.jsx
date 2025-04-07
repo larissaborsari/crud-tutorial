e65a2d74745cfa84f1d5907d991b841413824d45
Larissa <larissaborsari@hotmail.com>
2025-04-07 14:28:47 -0300

    first commit

---
+++ crud-frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Navbar from './components/Navbar'
+
+function App() {
+
+  return (
+    <>
+    <html data-theme="retro">
+      <div>
+       <Navbar />
+      </div>
+    </html>
+    </>
+  )
+}
+
+export default App
